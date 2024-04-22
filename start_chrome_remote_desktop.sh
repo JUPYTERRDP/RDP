e14096a3239858bb8d7a888677ed478122fe15a9
JUPYTERRDP <167531064+JUPYTERRDP@users.noreply.github.com>
2024-04-22 07:42:52 +0530

    Update start_chrome_remote_desktop.sh

--- start_chrome_remote_desktop.sh
+++ start_chrome_remote_desktop.sh
@@ -4,8 +4,7 @@
 sudo service chrome-remote-desktop stop
 
 # Start the Chrome Remote Desktop service with the CRP token
-DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AeaYSHBJvGI_Z-4ecqETr0LIyWEa4a6ifuk5Y9haaUx2kzIQvJSFVJYrwQPYRKEDKqDiOw" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
-
+DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AeaYSHDN1Q1XmTyl8G7WRL5Sz6m3TedHNqiyh2K_15mYi2Nv0cufpwdbVtbE9IPHRLwJAA" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
 # Check if the Chrome Remote Desktop service started successfully
 if [ $? -eq 0 ]; then
     echo "Chrome Remote Desktop host started successfully."
